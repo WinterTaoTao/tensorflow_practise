aae725017f895a146bbd3bf7483a00899c08f093
Winter <xwhwt92@hotmail.com>
2017-11-29 11:13:05 +1000

    mnist_deep

--- test.py
+++ test.py
@@ -2,12 +2,7 @@ import tensorflow as tf
 
 sess = tf.Session()
 
-x = tf.constant([[[1, 3], [2, 4], [3, 5]], [[4, 6], [5, 7], [6, 8]]])
-tf.reduce_sum(x)  # 6
-tf.reduce_sum(x, 0)  # [2, 2, 2]
-tf.reduce_sum(x, 1)  # [3, 3]
-tf.reduce_sum(x, 1, keep_dims=True)  # [[3], [3]]
-tf.reduce_sum(x, [0, 1])  # 6
+c = tf.truncated_normal(shape=[5, 10, 8, 2], mean=0, stddev=1)
 
-print(sess.run(tf.reduce_sum(x, [0, 1])))
-print(sess.run(x*x))
+sess = tf.Session()
+print sess.run(c)
